e0451da894348d09fe162b1eaa4a8a37cc1d95c5
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-02 13:38:51 +0530

    1.student-crud: [feat] fetching all student from frontend

--- 1.student-crud/studentfrontend/src/components/Student.js
+++ 1.student-crud/studentfrontend/src/components/Student.js
@@ -3,16 +3,35 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button, Container, Paper} from '@mui/material';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Student() {
   const paperStyle={padding:'50px 20px', width:600, margin:"20px auto"}
   const[name, setName] = useState('')
   const[address, setAddress] = useState('')
+  const[students, setStudents]=useState([])
   const handleClick=(e)=>{
     e.preventDefault()
     const student = {name, address}
     console.log(student)
+    fetch("http://localHost:8080/student/add", {
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(student)
+    }).then(()=>{
+      console.log("New Student added")
+    })
   }
+
+  useEffect(()=>{
+    fetch("http://localHost:8080/student/getAll")
+    .then(res=>res.json())
+    .then((result)=>{
+      setStudents(result);
+    }
+    )
+  },[])
+
   return (
     <Container>
       <Paper elevation={3} style={paperStyle}>
@@ -37,6 +56,18 @@ export default function Student() {
         Submit
       </Button>
     </Box>
+
+    </Paper>
+    <h1>Students</h1>
+
+    <Paper elevation={3} style={paperStyle}>
+      {students.map(student=>(
+        <Paper elevation={6} style={{margin:"10px",padding:"15px",textAlign:"left"}} key={student.id}>
+          Id:{student.id}<br/>
+          Name:{student.name}<br/>
+          Address:{student.address}
+    </Paper>
+      ))}
     </Paper>
     </Container>
   );
