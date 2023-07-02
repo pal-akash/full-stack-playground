60a7b1238bde33b0f7b2afd7feb93b29eeb804cf
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-02 13:11:33 +0530

    1.student-crud: [feat] saving from frontend

---
+++ 1.student-crud/studentfrontend/src/components/Student.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button, Container, Paper} from '@mui/material';
+import { useState } from 'react';
+
+export default function Student() {
+  const paperStyle={padding:'50px 20px', width:600, margin:"20px auto"}
+  const[name, setName] = useState('')
+  const[address, setAddress] = useState('')
+  const handleClick=(e)=>{
+    e.preventDefault()
+    const student = {name, address}
+    console.log(student)
+  }
+  return (
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
+        <h1 style={{color:"blue"}}>Add Student</h1>
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1},
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+      value={name}
+      onChange = {(e)=>setName(e.target.value)}
+      />
+      <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}
+      />
+      <Button variant='contained' color='secondary' onClick={handleClick}>
+        Submit
+      </Button>
+    </Box>
+    </Paper>
+    </Container>
+  );
+}
